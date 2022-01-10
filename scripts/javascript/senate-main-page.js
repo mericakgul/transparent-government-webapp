c5037922b24e4db52c2ec5abb12a464a44141143
mericakgul <makgul@worth.systems>
2022-01-10 16:40:46 +0100

    Adding filter by party of the checkboxes and cleaning up the code

--- scripts/javascript/senate-main-page.js
+++ scripts/javascript/senate-main-page.js
@@ -1,44 +1,78 @@
 
 import {data} from '../data/senateData.mjs';
 
-const wholeSenateData = data.results[0].members;  // This gets the whole data about only members from json file
+Array.from(document.querySelectorAll('.form-check-input'))
+    .forEach(element => element
+        .addEventListener('click', (event) => {
+            console.log('event: ', event);
+            initTable();
+            // alert('test message')
+}));
+
+function initTable () {
+    document.getElementById('senate-data').innerHTML = '';
+    const {members} = data.results[0]; // data.results[0].members; // This gets the whole data about only members from json file
+
+    const checkedParties = Array.from(document.querySelectorAll('.form-check-input'))
+        .filter(({checked}) => checked)
+        .map(({value}) => value);
 
 // This one filters the data about the members to have only the required data like name, state, party ...
 // [{name: xxx, party: D, state: MI,....}, {name: yyy, party: R, state: NY,....}, {}, .... {}] 102 objects inside the array and
 // each object has these properties: name, party, state, yearsInOffice, votePercentageWithParty and linkUrl.
-const summarySenateData = wholeSenateData.map(member => ({name: `${member.first_name} ${member.last_name}`,
-    party: member.party, state: member.state, yearsInOffice: member.seniority, votePercentageWithParty: member.votes_with_party_pct, linkUrl: member.url}));
+    const summarySenateData = members
+                                .filter(member => checkedParties.includes(member.party))
+                                .map(member => ({name: `${member.first_name} ${member.last_name}`,
+                                                party: member.party,
+                                                state: member.state,
+                                                yearsInOffice: member.seniority,
+                                                votePercentageWithParty: member.votes_with_party_pct,
+                                                linkUrl: member.url}));
 
-const  tableBody = document.getElementById('senate-data'); // senate-data is the id of the table body element in html
+    console.log('summarySenateData: ', summarySenateData);
 
-summarySenateData.forEach((member) =>  {         // member = {name: xxx, party: D, state: MI,....} and getting the next object in each iteration
+    createTable(summarySenateData);
+}
+
+function createTable (tableData) {
+    tableData.forEach(createTableRow);
+}
+
+function createTableRow(member) {    // member = {name: xxx, party: D, state: MI,....} and getting the next object in each iteration
+    const  tableBody = document.getElementById('senate-data'); // senate-data is the id of the table body element in html
     const tableRow = document.createElement('tr');
     Object.values(member).forEach((value, index, array) => {   // Object.values(member) is ['xxx', 'D', 'MI,....]
-        if (index < array.length-1){             // This condition is to filter the urls not to show in the table. Only one array has been created (summarySenateData).
-                                                 // Another array for the urls wasn't created to have the consistent.
+        if (index < array.length-1){                // This condition is to filter the urls not to show in the table. Only one array has been created (summarySenateData).
+                                                    // Another array for the urls wasn't created to have the consistent.
             const rowCell = document.createElement('td');
-            if (index === 0){         // adding the links to the names
-                const anchorTag = document.createElement('a');
-                const anchorTagText = document.createTextNode(value);
-                anchorTag.href = array[array.length-1];
-                anchorTag.target = '_blank';
-                anchorTag.appendChild(anchorTagText);
-                rowCell.appendChild(anchorTag);
-                tableRow.appendChild(rowCell);
-            }
-            else  {
-                const rowCellText = document.createTextNode(value);
-                rowCell.appendChild(rowCellText);
-                tableRow.appendChild(rowCell);
-            }
+            const rowCellText = (index === 0)
+                ? createAnchorTag(value, array[array.length-1])
+                : document.createTextNode(value);
+            rowCell.appendChild(rowCellText);
+            tableRow.appendChild(rowCell);
         }
     })
     tableBody.appendChild(tableRow);
-})
+}
 
-const filterSenateDataByParty = (party) => summarySenateData.filter(member => member.party === party);
+function createAnchorTag(anchorText, urlValue) {
+    const anchorTag = document.createElement('a');
+    const anchorTagText = document.createTextNode(anchorText);
+    anchorTag.href = urlValue;
+    anchorTag.target = '_blank';
+    anchorTag.appendChild(anchorTagText);
+    return anchorTag;
+}
 
-console.log('democrats: ', filterSenateDataByParty('ID'));
+
+initTable();
+
+
+
+
+// const filterSenateDataByParty = (party) => summarySenateData.filter(member => member.party === party);
+//
+// console.log('democrats: ', filterSenateDataByParty('ID'));
 
 
 
